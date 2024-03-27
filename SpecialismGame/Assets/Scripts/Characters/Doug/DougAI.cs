b7c128d471bb21108dd480336af01875bdc140c8
Connrooo <125368828+Connrooo@users.noreply.github.com>
2024-03-27 17:13:27 +0000

    Finished base coding of doug AI
    still need to add physical objects for doug to go up to

--- SpecialismGame/Assets/Scripts/Characters/Doug/DougAI.cs
+++ SpecialismGame/Assets/Scripts/Characters/Doug/DougAI.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public bool dougWalking;
     float objectFindTimer;
     float dougWaitTimer = 180;
     GameManagerStateMachine gameManager;
@@ -14,18 +14,27 @@ public class NewBehaviourScript : MonoBehaviour
     InteractDougManager[] interactDougManagers;
     NavMeshAgent dougNavMesh;
     DougAnimationScript dougAnimationScript;
+    Transform dougGoTo;
+    Transform defaultPosition;
+    Quaternion defaultRotation;
+    Quaternion targetRotation;
+    bool resetRotation;
+    Vector3 rotSmoothDampReference;
     
     private void Start()
     {
+        defaultRotation = transform.rotation;
         gameManager = FindObjectOfType<GameManagerStateMachine>();
         interactDougManagers = FindObjectsOfType<InteractDougManager>();
         dougNavMesh = GetComponent<NavMeshAgent>();
-        dougAnimationScript = GetComponent<DougAnimationScript>();
+        dougAnimationScript = FindObjectOfType<DougAnimationScript>();
+        StartCoroutine(TriggerIdle());
     }
 
     private void Update()
     {
         DougCounter();
+        IsRotationCorrect();
     }
 
     private void DougCounter()
@@ -36,7 +45,7 @@ public class NewBehaviourScript : MonoBehaviour
         }    
         else
         {
-            if (!dougWalking)
+            if (!dougAnimationScript.dougWalking)
             {
                 objectFindTimer += Time.deltaTime;
             }
@@ -47,26 +56,76 @@ public class NewBehaviourScript : MonoBehaviour
         }
     }
 
-    private void LookForObject()
+    private void IsRotationCorrect()
     {
-        objectSelected = interactDougManagers[Random.Range(0, interactDougManagers.Length)].gameObject;
-        dougNavMesh.destination = objectSelected.transform.position;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.transform == objectSelected.transform)
+        if (!dougAnimationScript.dougWalking&&resetRotation)
         {
-            dougAnimationScript.dougPickup = true;
+            transform.rotation = Quaternion.Euler(Vector3.SmoothDamp(transform.rotation.eulerAngles, targetRotation.eulerAngles, ref rotSmoothDampReference, 200 * Time.deltaTime));
+            if (transform.rotation == defaultRotation)
+            {
+                resetRotation = false;
+            }
+
         }
     }
 
-    private void FoundObject()
+    IEnumerator TriggerIdle()
+    {
+        yield return new WaitForSeconds(Random.Range(5,30));
+        dougAnimationScript.dougAnimator.SetInteger("InteruptAnimation", Random.Range(1, 7));
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(TriggerIdle());
+    }
+
+
+
+    private void LookForObject()
+    {
+        if (interactDougManagers[0]!=null)
+        {
+            objectSelected = interactDougManagers[Random.Range(0, interactDougManagers.Length)].gameObject;
+            dougGoTo = objectSelected.transform;
+            dougAnimationScript.dougWalking = true;
+        }
+        else
+        {
+            objectFindTimer = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.transform == objectSelected.transform)
+        {
+            dougAnimationScript.dougPickup = true;
+            currentObject = objectSelected.GetComponent<InteractDougManager>().itemScript.gameObject;
+            targetRotation = Quaternion.LookRotation(currentObject.transform.position);
+            resetRotation = true;
+        }
+        else if (col.transform == defaultPosition.transform)
+        {
+            dougAnimationScript.dougWalking = false;
+            objectFindTimer = 0;
+            targetRotation = defaultRotation;
+            resetRotation = true;
+        }
+    }
+
+    public void FoundObject()
     {
         var clueScript = currentObject.GetComponent<ClueScript>();
         gameManager.pickedUpObjects.Add(clueScript.pickup);
+        dougGoTo = defaultPosition;
         Destroy(currentObject);
         Destroy(objectSelected);
+    }
+
+    private void DougTargetPosition()
+    {
+        if (dougAnimationScript.dougWalking)
+        {
+            dougNavMesh.destination = dougGoTo.position;
+        }
     }
 
 }
--- SpecialismGame/Assets/Scripts/Characters/Doug/InteractDougManager.cs
+++ SpecialismGame/Assets/Scripts/Characters/Doug/InteractDougManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InteractDougManager : MonoBehaviour
 {
-    ClueScript itemScript; 
+    public ClueScript itemScript; 
     // Start is called before the first frame update
     void Start()
     {
