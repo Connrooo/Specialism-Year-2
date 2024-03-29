077dbc3fdff33f6aae32e1f8b711cfc2bad3d1e1
Connrooo <125368828+Connrooo@users.noreply.github.com>
2024-03-29 20:20:00 +0000

    Added most models to rooms

--- SpecialismGame/Assets/Scripts/Characters/PoliceChiefScript.cs
+++ SpecialismGame/Assets/Scripts/Characters/PoliceChiefScript.cs
@@ -6,19 +6,28 @@ public class PoliceChiefScript : MonoBehaviour
 {
     [SerializeField] Vector3[] roomPositions;
     GameManagerStateMachine gameManager;
+    Vector3 startingRotation;
+    GameObject Player;
     private void Start()
     {
+        Player = GameObject.FindGameObjectWithTag("Player");
         gameManager = FindObjectOfType<GameManagerStateMachine>();
+        startingRotation = transform.rotation.eulerAngles;
     }
     private void Update()
     {
+        transform.LookAt(Player.transform.position);
+        var currentRotation = transform.rotation.eulerAngles;
+        currentRotation.x = startingRotation.x;
+        currentRotation.z = startingRotation.z;
+        transform.rotation = Quaternion.Euler(currentRotation);
         if (gameManager.inRoom)
         {
             transform.position = roomPositions[gameManager.currentRoomNumber-1];
         }
         else if (gameManager.inDeliberation)
         {
-            if (gameManager.accusingSuspect)
+            if (gameManager.accusingSuspect || gameManager.day == 3)
             {
                 transform.position = new Vector3(0, 0.025f, 3.5f);
             }
